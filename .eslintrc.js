118c0f9da31ce9ab47b6a16307f964b665b69ee0
goodness5 <goodnesskolapo@gmail.com>
2026-02-09 00:18:49 +0100

    upgrade nodejs version

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,5 @@
 module.exports = {
-  extends: [
-    'react-app',
-    'react-app/jest'
-  ],
-  rules: {
-    'react-hooks/exhaustive-deps': 'off',
-    'array-callback-return': 'off',
-    'no-unused-vars': 'off',
-    'no-useless-escape': 'off',
-    'jsx-a11y/alt-text': 'off',
-    'jsx-a11y/anchor-is-valid': 'off'
-  }
+  extends: [],
+  rules: {}
 };
 
